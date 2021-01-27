6f7a18730e0e613fe2ff74d499d4b0a08790888a
Zane Wolf <30910417+zanewolf@users.noreply.github.com>
2021-01-26 21:13:47 -0500

    Initial commit

---
+++ app/components/Popular.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+function LanguagesNav({selected, onUpdateLanguage}){
+        const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
+
+        return(
+            <ul className='flex-center'>
+                {languages.map((language)=>(
+                    <li key={language}>
+                        <button
+                            className='btn-clear nav-link'
+                            style={language === selected ? {color: 'red' } :null}
+                            onClick = {()=> onUpdateLanguage(language)}>
+                            {language}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        )
+
+}
+export default class Popular extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            selectedLanguage: 'All'
+        }
+
+        this.updateLanguage = this.updateLanguage.bind(this)
+    }
+
+    updateLanguage(selectedLanguage){
+        this.setState({
+            selectedLanguage
+        })
+        console.log(selectedLanguage)
+    }
+
+    render(){
+        const {selectedLanguage} = this.state
+
+        return (
+        <React.Fragment>
+            <LanguagesNav
+                selected = {selectedLanguage}
+                onUpdateLanguage = {this.updateLanguage}
+            />
+        </React.Fragment>
+        )
+
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports={
+    entry: './app/index.js', //the entry point is the file that kicks everything off and is often index.js
+    module:{
+        rules: [
+            { test: /\.svg$/, use: 'svg-inline-loader'}, //if it has the svg file path, use this loader
+            {test: /\.css$/, use: ['style-loader', 'css-loader']}, //using two loaders, so use array, and the array is processed in reverse order
+            {test: /\.(js)$/, use: 'babel-loader'}
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js' //create dist folder and inside dist folder, add the bundle of files and name it index_bundle.js
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'app/index.html'
+            })
+        // new webpack.EnvironmentPlugin({
+        //     'NODE_ENV': 'production'
+        // })
+    ],
+    mode: "development"
+}
